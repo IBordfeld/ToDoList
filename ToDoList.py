68427f86d2188ba343cf72e3fc6c106b6689fc03
IBordfeld <56699699+IBordfeld@users.noreply.github.com>
2020-11-27 18:12:26 -0500

    Add files via upload

--- ToDoList.py
+++ ToDoList.py
@@ -2,20 +2,88 @@
 # ToDoList
 
 class ToDoList:
-    def __init__(self, category, listName):
-        self.Category = category
-        self.Name = listName
-        self.ToDo = list()
+    def __init__(self):
+        self.ToDo = {}
+        self.OpenList()
 
-    def addToList(self, task):
-        self.ToDo.append(task)
+    def addParentList(self, ListName):
+        self.ToDo[ListName] = list()
 
-    def removeFromToDoList(self, task):
-        if task in self.ToDo:
-            self.ToDo.remove(task)
+    def removeParentList(self, ListName):
+        del self.ToDo[ListName]
 
-    def print_list(self):
-        printed_list = ""
-        for item in self.ToDo:
-            printed_list += "- {}\n".format(item)
-        return printed_list
+    def addToDoList(self, ListName, task):
+        self.ToDo.get(ListName).append(task)
+
+    def removeFromToDoList(self, ListName, task):
+        self.ToDo.get(ListName).remove(task)
+
+    def SaveLists(self):
+        fptr = open("ToDo.txt", "w")
+
+        for listName, task in self.ToDo.items():
+            fptr.write(listName + ":")
+            run_time = len(task)
+            for i in range(run_time - 1):
+                fptr.write(task[i] + ",")
+            fptr.write(task[-1] + "\n")
+
+        fptr.close()
+        print("List saved! Have a good day!")
+
+    def OpenList(self):
+        try:
+            fptr = open("ToDo.txt", "r")
+        except Exception as identifier:
+            ...
+
+        for ToDo in fptr.readlines():
+            tempList = ToDo.split(":")
+            tempList = [tempList[0], tempList[1].strip("\n").split(",")]
+            self.ToDo[tempList[0]] = tempList[1]
+
+        fptr.close()
+
+    def printList(self):
+        listString = ""
+        for name, listItems in self.ToDo.items():
+            listString += name + ":\n"
+            for task in listItems:
+                listString += f"- {task}\n"
+        return listString[:-1]
+
+    def getList(self):
+        return self.ToDo
+
+"""
+Here are your lists
+    1. Store
+    2. Chores
+    3. Remove a list
+    4. Add a list
+    5. Save List and Exit
+
+SELECTS 1
+
+Store:
+    - Eggs
+    - Milk
+    - Bread
+    - Poptarts
+    1. Add to list
+    2. Remove from List
+    3. Go back to all my lists
+"""
+
+
+# test = ToDoList()
+# test.addParentList("Store")
+# test.addToDoList("Store", "Eggs")
+# test.addToDoList("Store", "Milk")
+# test.addToDoList("Store", "Bread")
+# test.addToDoList("Store", "Poptarts")
+# test.addParentList("Chores")
+# test.addToDoList("Chores", "Clean")
+# test.addToDoList("Chores", "Pet cat")
+# test.addToDoList("Chores", "Cook")
+# test.SaveLists()
--- main.py
+++ main.py
@@ -1,18 +1,90 @@
-import tkinter as tk
-import ToDoList
+from ToDoList import ToDoList
+
+# test = ToDoList()
+# test.addParentList("Store")
+# test.addToDoList("Store", "Eggs")
+# test.addToDoList("Store", "Milk")
+# test.addToDoList("Store", "Bread")
+# test.addToDoList("Store", "Poptarts")
+# test.addParentList("Chores")
+# test.addToDoList("Chores", "Clean")
+# test.addToDoList("Chores", "Pet cat")
+# test.addToDoList("Chores", "Cook")
+# print(test.printList())
+# test.removeFromToDoList("Chores", "Pet cat")
+# print(test.printList())
+
+class Menu:
+    def __init__(self):
+        self.MyList = ToDoList()
+        self.state = "Start"
+
+    def showMenu(self):
+        menuString = self.StartMenu()
+        while 1:
+            print(menuString)
+            user = int(input("Select Option: "))
+            
+            if user == 9:
+                exit()
+
+            
+            if self.state == "Start":
+                option = self.findOption(user) # Option that user selected in start menu
+                if option >= len(self.MyList.getList()): # If user's option was a list
+                    menuString = self.CurrentList(option)
+                    self.state = "Current"
+                else: # If any of the extra options were selected
+                    ...
+            elif self.state == "Current":
+                ...
+
+    def StartMenu(self):
+        position = 1
+        startMenuString = "Your current Lists:\n"
+        for ListName, task in self.MyList.getList().items():
+            startMenuString += f"{position}. " + ListName + "\n"
+            position += 1
+
+        for extraOptions in self.getStateOptions():
+            startMenuString += f"{position}" + extraOptions + "\n"
+            position += 1
+
+        return startMenuString
+
+    def CurrentList(self, ListName):
+        myListString = f"{ListName}:\n"
+        listSelected = self.MyList.getList().get(ListName)
+        for task in listSelected:
+            myListString += f"- {task} \n"
+
+        position = 1
+        for extraOptions in self.getStateOptions():
+            myListString += f"{position}" + extraOptions + "\n"
+            position += 1
+
+        return myListString
+
+    def findOption(self, option):
+        optionPosition = 1
+        for ListName, task in self.MyList.getList().items():
+            if optionPosition == option:
+                # Open list that was selected (if one was, other three options could have been selected)
+                return ListName
+            optionPosition += 1
+
+        return option
+
+    def findExtraOption(self):
+        ...
+
+    def getStateOptions(self):
+        if self.state == "Start":
+            return [". Add a list", ". Remove a List", ". Save Lists and Exit"]
+        else:
+            return [". Add to list", ". Remove from List", ". Go back to all my lists"]
 
 
-def menu():
-    menu_selector = ""
-    test = ToDoList.ToDoList("home", "Groceries")
-    test.addToList("Milk")
-    test.addToList("cookies")
-    test.addToList("cake")
-    while menu_selector is not "q":
-        menu_selector = input("Option: ")
-        if menu_selector == "1":
-            print(test.print_list)
-
-
-if __name__ == '__main__':
-    menu()
+test = Menu()
+test.MyList.addParentList("Homework")
+test.showMenu()
